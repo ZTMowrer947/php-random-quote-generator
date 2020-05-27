aa2022a0e77645555b0bf6b3eaf19b3af723a7ac
Zackery Mowrer <Zackmowrer@gmail.com>
2020-05-27 11:11:30 -0400

    Add quotes array

--- inc/functions.php
+++ inc/functions.php
@@ -1,11 +1,51 @@
+<?php
 // PHP - Random Quote Generator
 
-// Create the Multidimensional array of quote elements and name it quotes
-// Each inner array element should be an associative array
+// Declare array of quote objects
+$quotes = [
+    [
+        // The quote itself
+        "quote" => "Try not. Do. Or do not. There is no try.",
+        
+        // The author of the quote
+        "source" => "Yoda",
 
+        // The origin of the quote
+        "citation" => "Star Wars: Episode V - The Empire Strikes Back",
 
+        // The year of the quote
+        "year" => 1980,
 
-// Create the getRandomQuuote function and name it getRandomQuote
+        // Tags relating to this quote
+        "tags" => ["Star Wars", "Film"]
+    ],
+    [
+        "quote" => "Only a Sith deals in absolutes.",
+        "source" => "Obi-Wan Kenobi",
+        "citation" => "Star Wars: Episode III - Revenge of the Sith",
+        "year" => 2005,
+        "tags" => ["Star Wars", "Film"]
+    ],
+    [
+        "quote" => "The only thing we have to fear is fear itself.",
+        "source" => "Franklin D. Roosevelt",
+        "citation" => "Inaugural Address of the President of the United States",
+        "year" => 1933,
+        "tags" => ["United States", "Presidential"]
+    ],
+    [
+        "quote" => "Never, never, never give up.",
+        "source" => "Winston Churchill",
+        "tags" => ["United Kingdom", "WWII", "Inspirational"]
+    ],
+    [
+        "quote" => "It does not matter how slowly you go as long as you do not stop.",
+        "source" => "Confucius",
+        "tags" => ["China", "Inspirational"]
+    ]
+];
+
+// Create the getRandomQuote function and name it getRandomQuote
 
 
 
